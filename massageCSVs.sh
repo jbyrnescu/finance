3dfde167ba40d83c99bd6543c0628937569c784c
John Byrne <jbyrnescu@me.com>
2022-02-04 13:49:55 -0800

    changed a few more lines after changing a few lines from download

--- massageCSVs.sh
+++ massageCSVs.sh
@@ -2,7 +2,7 @@
 
 # we change directory to where the csv files have been copied
 
-echo Ive been given a directory to change to where the csv files are:
+#echo Ive been given a directory to change to where the csv files are:
 echo Changing directory to: $1
 cd $1
 
@@ -22,10 +22,10 @@ sed -i -e '1 i \
 TransactionNumber,TransactionDate,Memo,Description,DebitAmount,CreditAmount,Balance,CheckNumber,Fees,BudgetCat' SavingsStarOneTXs.csv
 
 # remove 1st 4 lines of Barclays because it's header info
-sed -i -e "1,4 d" MastercardBarclaysJetsTXs.csv
+#sed -i -e "1,4 d" MastercardBarclaysJetsTXs.csv
 
-sed -i -e 's/Transaction Date/TransactionDate/' MastercardBarclaysJetsTXs.csv
-sed -i -e 's/Category/BudgetCat/' MastercardBarclaysJetsTXs.csv
+#sed -i -e 's/Transaction Date/TransactionDate/' MastercardBarclaysJetsTXs.csv
+#sed -i -e 's/Category/BudgetCat/' MastercardBarclaysJetsTXs.csv
 
 # change the Visa cc headers
 
@@ -34,14 +34,14 @@ sed -i -e 's/Post Date/PostDate/' VisaChaseTXs.csv
 
 
 #add column labels/names to WellsFargo
-sed -i -e '1 i \
-TransactionDate,Amount,Unknown1,Unknown2,Description,BudgetCat\
-' CheckingWellsFargoTXs.csv
+#sed -i -e '1 i \
+#TransactionDate,Amount,Unknown1,Unknown2,Description,BudgetCat\
+#' CheckingWellsFargoTXs.csv
 
 #add column labels/names to WellsFargo (CreditCard)
-sed -i -e '1 i \
-TransactionDate,Amount,Unknown1,Unknown2,Description,BudgetCat\
-' VisaWellsFargoTXs.csv
+#sed -i -e '1 i \
+#TransactionDate,Amount,Unknown1,Unknown2,Description,BudgetCat\
+#' VisaWellsFargoTXs.csv
 
 dateSuffix=`date "+%Y%m%d%H%M"`
 
@@ -54,9 +54,9 @@ sed -i -e 's/\/Users\/jbyrne\/TeamRex Dropbox\/John Byrne\/ScannedDocs\/2021_05_
 # now... add the header so the friggin' database import prog. knows
 # header: TransactionDate, Amount, Description, TransactionType
 
-sed -i -e '1 i \
-TransactionDate,Amount,Description,TransactionType
-' MastercardCitibankShellTXs.csv
+#sed -i -e '1 i \
+#TransactionDate,Amount,Description,TransactionType
+#' MastercardCitibankShellTXs.csv
 
 cat GimportCSVs${dateSuffix}.sql | sqlite3 -echo -batch
 
