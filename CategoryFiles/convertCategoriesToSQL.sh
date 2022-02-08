6890853aa4082184dcf01548969d5a58acd6a31e
John Byrne <jbyrnescu@me.com>
2022-02-08 14:01:16 -0800

    Working version of SQL generator working. Still need to put it in the correct directory and a few other things. Like... check that the categorization isn't making any mistakes.  (Test Categorized.csv & the SQL statement generated) Made convert... a for statement... ahhh... much better.  Need to clean up comments and code not used.

--- CategoryFiles/convertCategoriesToSQL.sh
+++ CategoryFiles/convertCategoriesToSQL.sh
@@ -5,29 +5,38 @@ stmt2="where Description like \"%"
 stmt3="%\""
 
 tble1="CheckingStarOneTXs"
-tble2="CheckingWellsFargoTXs"
-tble3="MastercardBarclaysJetsTXs"
-tble4="MastercardCitibankShellTXs"
-tble5="SavingsStarOneTXs"
-tble6="VisaChaseTXs"
+tble2="SavingsStarOneTXs"
+tble3="CheckingWellsFargoTXs"
+
+#tble3="MastercardBarclaysJetsTXs"
+#tble4="MastercardCitibankShellTXs"
+#tble6="VisaChaseTXs"
 
 #echo CHECK
 #echo $stmt1 $stmt2 $stmt3
 
+rm categorizeTXs.sql
+
+for curBankAcct in $tble1 $tble2 $tble3
+do
+    cat Categorized.csv | sed -E -e "s/^([^,]*),(.*)$/ update $curBankAcct set BudgetCat=\"\\2\" where Description like \"%\\1%\"; /" >> categorizeTXs.sql
+    echo --------------------------------------------- >> categorizeTXs.sql
+done
+
 #head -n 2 Categorized.csv 
 #echo
 #echo
-cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update CheckingWellsFargoTXs set BudgetCat="\2" where Description like "%\1%"; /' > categorizeTXs.sql
-echo --------------------------------------------- >> categorizeTXs.sql
-cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update CheckingWellsFargoTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
-echo --------------------------------------------- >> categorizeTXs.sql
-cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update MastercardBarclaysJetsTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
-echo --------------------------------------------- >> categorizeTXs.sql
-cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update MastercardCitibankShellTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
-echo --------------------------------------------- >> categorizeTXs.sql
-cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update SavingsStarOneTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
-echo --------------------------------------------- >> categorizeTXs.sql
-cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update VisaChaseTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
+# cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update CheckingWellsFargoTXs set BudgetCat="\2" where Description like "%\1%"; /' > categorizeTXs.sql
+# echo --------------------------------------------- >> categorizeTXs.sql
+# cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update CheckingWellsFargoTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
+# echo --------------------------------------------- >> categorizeTXs.sql
+# cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update MastercardBarclaysJetsTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
+# echo --------------------------------------------- >> categorizeTXs.sql
+# cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update MastercardCitibankShellTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
+# echo --------------------------------------------- >> categorizeTXs.sql
+# cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update SavingsStarOneTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
+# echo --------------------------------------------- >> categorizeTXs.sql
+# cat Categorized.csv | sed -E -e 's/^([^,]*),(.*)$/ update VisaChaseTXs set BudgetCat="\2" where Description like "%\1%"; /' >> categorizeTXs.sql
 
 
 
