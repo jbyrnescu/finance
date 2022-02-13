d4dd4081f4be3cf1c00ac792f448a41033746050
John Byrne <jbyrnescu@yahoo.com>
2022-02-13 10:49:55 -0800

    Update percentCategorized.sql
    too many categorized txs led me to add this.  is not null with budgetcat column

--- Queries/percentCategorized.sql
+++ Queries/percentCategorized.sql
@@ -1,4 +1,4 @@
-select count(*) from BigTXView where BudgetCat IS NOT "" ;
+select count(*) from BigTXView where BudgetCat IS NOT "" and BudgetCat is not null;
 select "OUT OF:";
 select count(*) from BigTXView where 1=1 ; 
 select "RECORDS";
