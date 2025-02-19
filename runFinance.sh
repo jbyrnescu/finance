d2376f4fedd02f5bb54cd2d28c32e96ffa6bb6ae
John Byrne <jbyrnescu@yahoo.com>
2025-02-19 03:20:39 -0800

    Added SavingsPercentages.csv and runFinance.java

---
+++ runFinance.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp ${HOME}/Dropbox/eclipse-workspace/finance/target/finance-0.0.1-SNAPSHOT-jar-with-dependencies.jar finance.Finance ${HOME}/Dropbox/eclipse-workspace/finance ~/Downloads
