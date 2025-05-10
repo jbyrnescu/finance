611ea83a689256f16266b23f6ec05972c5b58abd
John Byrne <jbyrnescu@yahoo.com>
2025-05-10 01:57:39 -0700

    Added perl script to convert DollarsPerDayExpenditures.csv to Monthly version, DollarsPerMonth.csv

---
+++ budgetStatus/MnthlyToDrsPD.pl
@@ -0,0 +1,31 @@
+#!/opt/homebrew/bin/perl
+
+use warnings;
+use strict;
+
+my $dailyAmountFilename = "../DollarsPerDayExpenditures.csv";
+my $mnthlyAmountFilename = "../DollarsPerMonth.csv";
+
+open(my $fh,"<",$mnthlyAmountFilename) or die "Can't open $mnthlyAmountFilename";
+
+my $line = <$fh>;
+
+chomp ($line);
+
+my $title;
+
+(my $date, $title) = split(',',$line);
+
+printf("Date: %s , %s - Converted to DAILY (dollars per day)\n", $date, $title);
+
+chomp(my @lines = <$fh>);
+
+my @sorted_lines = sort @lines;
+#{ lines->[1] cmp lines->[1] } 
+
+foreach $line (@sorted_lines) {
+    my ( $amount, $category ) = split (',',$line);
+    $amount =~ s/\"//g;
+    printf("%s,%-.2f\n", uc $category, $amount/31);
+}
+
